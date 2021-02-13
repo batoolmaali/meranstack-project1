03a398d661b4908bcf3964323be254994e33ef62
unknown <batool.s.maali@gmail.com>
2021-02-13 23:30:17 +0300

    social network

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const connectDB= require('./config/db')
 const app = express();
-
+ connectDB();
 app.get('/',(req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./route/api/users'));
+app.use('/api/profile', require('./route/api/profile'));
+app.use('/api/post', require('./route/api/post'));
+app.use('/api/auth', require('./route/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
